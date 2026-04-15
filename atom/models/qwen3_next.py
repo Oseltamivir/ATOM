3f975ca89ce7147e58ffde796544e8d2a2502454
zejunchen-zejun <zejun.chen@amd.com>
2026-04-15 13:03:01 +0800

    add
    Signed-off-by: zejunchen-zejun <zejun.chen@amd.com>

--- atom/models/qwen3_next.py
+++ atom/models/qwen3_next.py
@@ -209,6 +209,8 @@ class Qwen3NextSparseMoeBlock(nn.Module):
             prefix=f"{prefix}.gate",
         )
 
+        # self.shared_expert_gate = torch.nn.Linear(config.hidden_size, 1, bias=False)
+
         if (
             config.shared_expert_intermediate_size > 0
             and not is_rocm_aiter_fusion_shared_expert_enabled()
